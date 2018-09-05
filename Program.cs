d45d59c64f97638bd0235ae15b46a157185bfd08
israr56 <42982630+israr56@users.noreply.github.com>
2018-09-04 20:52:26 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Name = "Nathan";
+           Console.WriteLine("Hello, {0}", Name);
+           // Console.Write("Hello, ");
+           // Console.Write("World" );
+
+            Console.WriteLine("Hello, " + Name);
+
+            string input = Console.ReadLine();
+            ConsoleKeyInfo cki;
+            do
+            {
+                cki = Console.ReadKey(true);
+                Console.Write("You Pressed:  ");
+                if ((cki.Modifiers & ConsoleModifiers.Alt) != 0) Console.Write("Alt+ ");
+                if ((cki.Modifiers & ConsoleModifiers.Shift) != 0) Console.Write("Shift+ ");
+                if ((cki.Modifiers & ConsoleModifiers.Control) != 0) Console.Write("Control+ ");
+                Console.WriteLine("{0} (character '{1}' )", cki.Key, cki.KeyChar);
+
+            }
+
+            while (cki.Key != ConsoleKey.Escape);
+        }
+    }
+}
